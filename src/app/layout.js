3c0e0ca4778178e53d76308f092c906fb5c5ecfb
hasan814 <h.mousavi910@gmail.com>
2024-06-13 23:15:12 +0330

    Start Project

---
+++ src/app/layout.js
@@ -0,0 +1,23 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/layout/Header";
+import Footer from "@/layout/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "TailAmazons App",
+  description: "Online Shopping",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+        <main className="">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
